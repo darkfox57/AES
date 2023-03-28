d9024de7ee98ad3fa91705cfa016fb8c34f7e15e
Andrés Pinto <andres.pinto.n@gmail.com>
2023-03-28 18:34:53 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
-import React, { useState } from 'react'
-
-
+import React from 'react'
+import { Route, Routes } from 'react-router'
 
 function App() {
-  return <div></div>
+  return (
+    <>
+      <Routes>
+        <Route path="/" />
+        <Route path="/acerca-de" />
+        <Route path="/involucrate" />
+        <Route path="/blog" />
+        <Route path="/blog:slug" />
+        <Route path="/mas" />
+        <Route path="/contacto" />
+      </Routes>
+    </>
+  )
 }
 
 export default App
