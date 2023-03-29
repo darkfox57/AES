641b89a38965c50b0e8e826341b4ac146fae2c32
mxv-0827 <115747349+mxv-0827@users.noreply.github.com>
2023-03-29 18:39:10 -0300

    Banner creado (#20)

---
+++ src/components/Banner_Cita/Banner.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import { Banner_Stylizied, PharaseContainer } from './Banner.Styles'
+export default function Banner() {
+  return (
+    <Banner_Stylizied>
+      <PharaseContainer>
+        <span>Sadhguru</span>
+        <p>
+          "En lugar de invertir solo en salud, debemos hablar de como generar
+          salud en una sociedad, una cultura de salud".
+        </p>
+      </PharaseContainer>
+    </Banner_Stylizied>
+  )
+}
