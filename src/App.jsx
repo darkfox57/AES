889c0e0952961b8562fa70fb88c658f34e0a73db
mxv-0827 <115747349+mxv-0827@users.noreply.github.com>
2023-03-29 12:44:07 -0300

    Creacion slider (#2)
    * Slider funcional creado
    
    * Creacion slider funcional

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import { Route, Routes } from 'react-router'
 
+import Home from './pages/Home/Home'
+
+//Hahber si asi funciona
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" />
+        <Route path="/" element={<Home />} />
         <Route path="/acerca-de" />
         <Route path="/involucrate" />
         <Route path="/blog" />
---
+++ src/components/Slider/Slider.Styles.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components'
+
+//Hahber si asi funciona
+
+export const Slider_Stylizied = styled.section`
+  background-image: url(${({ image }) => image});
+  color: var(--text-white);
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+
+  width: 100%;
+  min-height: 450px;
+  padding: 150px 0;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`
+
+export const Button_Stylizied = styled.button`
+  width: 50px;
+  height: 50px;
+`
