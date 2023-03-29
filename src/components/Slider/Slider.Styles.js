6613e256ec9bcbdeba815f341d2de70112a93607
mxv-0827 <maximilianoviand@gmail.com>
2023-03-29 13:33:37 -0300

    Boton Slider creado

--- src/components/Slider/Slider.Styles.js
+++ src/components/Slider/Slider.Styles.js
@@ -1,7 +1,5 @@
 import styled from 'styled-components'
 
-//Hahber si asi funciona
-
 export const Slider_Stylizied = styled.section`
   background-image: url(${({ image }) => image});
   color: var(--text-white);
@@ -10,15 +8,12 @@ export const Slider_Stylizied = styled.section`
   background-size: cover;
 
   width: 100%;
-  min-height: 450px;
+  min-height: 550px;
   padding: 150px 0;
 
   display: flex;
   justify-content: space-between;
   align-items: center;
-`
 
-export const Button_Stylizied = styled.button`
-  width: 50px;
-  height: 50px;
+  transition: background-image 1s ease-in-out;
 `
---
+++ src/utils/ArrowButton/ArrowButton.Styles.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+
+export const ArrowButton_Stylizied = styled.button`
+  border-radius: 20px;
+  width: 50px;
+  height: 50px;
+`
