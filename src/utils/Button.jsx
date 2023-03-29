10f71de424a795decd3ef70b162be6540e356e83
mxv-0827 <maximilianoviand@gmail.com>
2023-03-29 15:56:57 -0300

    Detalles y Boton en acciones_div

--- src/utils/Button.jsx
+++ src/utils/Button.jsx
@@ -4,10 +4,9 @@ import { ButtonStyle } from './button.styles'
 
 // se necesitan 4 argumentos que son los que se estan pasando por props
 
-// para ajustar colores estan las opciones primary, secondary y link la cual es para poner enlaces de leer más
+// para ajustar colores estan las opciones primary, secondary y link la cual es para poner enlaces de leer más. Esto se pasa en el prop 'type'
 
-// para ajustar colores estan las opciones primary y secondary
-// para seleccionar tamaños estan las opciones sm - md - lg   siendo sm la mas pequeña, lg la más grande y md la que se usa en la mayoria de los casos
+// para seleccionar tamaños estan las opciones sm - md - lg   siendo sm la mas pequeña, lg la más grande y md la que se usa en la mayoria de los casos. Esto se pasa en la prop 'size'
 
 export default function Button({ type, link, text, size }) {
   return (
