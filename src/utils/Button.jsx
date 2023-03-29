4f89ce3c562f033065e6d22509492914959a1a26
Andrés Pinto <andres.pinto.n@gmail.com>
2023-03-29 12:35:51 -0500

    Update Button.jsx

---
+++ src/utils/Button.jsx
@@ -0,0 +1,18 @@
+<<<<<<< Updated upstream
+=======
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { ButtonStyle } from './button.styles'
+
+// se necesitan 4 argumentos que son los que se estan pasando por props
+// para ajustar colores estan las opciones primary, secondary y link la cual es para poner enlaces de leer más
+// para seleccionar tamaños estan las opciones sm - md - lg   siendo sm la mas pequeña, lg la más grande y md la que se usa en la mayoria de los casos
+
+export default function Button({ type, link, text, size }) {
+  return (
+    <ButtonStyle type={type} size={size}>
+      <Link to={link}>{text}</Link>
+    </ButtonStyle>
+  )
+}
+>>>>>>> Stashed changes
