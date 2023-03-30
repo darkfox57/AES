f75c10e0feeca1e95db4462c213e712f7d97d8b4
Andrés Pinto <andres.pinto.n@gmail.com>
2023-03-30 14:55:59 -0500

    Estructura de layout para el dashboard (#26)

---
+++ src/DashBoard/Pages/AddBlog/AddBlog.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import DashLayout from '../../../layouts/DashLayout'
+
+export default function AddBlog() {
+  return (
+    <DashLayout>
+      <div>AddBlog</div>
+    </DashLayout>
+  )
+}
