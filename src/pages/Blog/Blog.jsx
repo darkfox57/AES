d2948722d96be2638049bb23b1eebc579b3af7e3
Andrés Pinto <andres.pinto.n@gmail.com>
2023-04-02 09:06:56 -0500

    cambio de redux a toolkit (#36)

--- src/pages/Blog/Blog.jsx
+++ src/pages/Blog/Blog.jsx
@@ -9,30 +9,27 @@ import { BlogBody } from './blog.styles'
 
 export default function Blog() {
   const dispatch = useDispatch()
-  const posts = useSelector((state) => state.blogs)
+  const posts = useSelector((state) => state.blog.blogs)
 
   useEffect(() => {
     dispatch(getAllBlogs())
   }, [dispatch])
 
   return (
-    console.log(posts),
-    (
-      <>
-        <Portada img={blogimg} titulo="Blog" />
-        <BlogBody>
-          {posts.map((post, i) => (
-            <BlogCard
-              key={post._id}
-              image={post.image}
-              title={post.title}
-              status={post.status}
-              date={post.createdAt}
-            />
-          ))}
-        </BlogBody>
-        <Footer />
-      </>
-    )
+    <>
+      <Portada img={blogimg} titulo="Blog" />
+      <BlogBody>
+        {posts.map((post, i) => (
+          <BlogCard
+            key={post._id}
+            image={post.image}
+            title={post.title}
+            status={post.status}
+            date={post.createdAt}
+          />
+        ))}
+      </BlogBody>
+      <Footer />
+    </>
   )
 }
---
+++ src/redux/reducer/blog_reducer.js
@@ -0,0 +1,41 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { getAllBlogs, getBlog, getCategories } from '../actions/actions';
+
+
+
+const initialState = {
+  blogs: [],
+  blog: {},
+  categories: [],
+  error: null,
+};
+
+
+const blogSlice = createSlice({
+  name: 'blogs',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(getAllBlogs.fulfilled, (state, action) => {
+        state.blogs = action.payload;
+      })
+      .addCase(getBlog.fulfilled, (state, action) => {
+        state.blog = action.payload;
+      })
+      .addCase(getCategories.fulfilled, (state, action) => {
+        state.categories = action.payload;
+      })
+      .addCase(getAllBlogs.rejected, (state, action) => {
+        state.error = action.error.message;
+      })
+      .addCase(getBlog.rejected, (state, action) => {
+        state.error = action.error.message;
+      })
+      .addCase(getCategories.rejected, (state, action) => {
+        state.error = action.error.message;
+      });
+  },
+});
+
+export default blogSlice.reducer;
