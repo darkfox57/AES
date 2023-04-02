bbeda731580c63096a04de250af9e001a8d505fe
mxv-0827 <maximilianoviand@gmail.com>
2023-04-02 11:08:22 -0300

    Form_Instituciones

--- src/pages/Involucrate/Involucrate.jsx
+++ src/pages/Involucrate/Involucrate.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import Portada from '../../components/Portada/Portada'
 import Form_Especialistas from '../../components/Form_Especialistas/Form_Especialistas'
-const img = "https://i.pinimg.com/originals/91/8f/49/918f490f7c1a15124850c009b605e2ee.jpg"
+import Form_Instituciones from '../../components/Form_Instituciones/Form_Instituciones'
+const img =
+  'https://i.pinimg.com/originals/91/8f/49/918f490f7c1a15124850c009b605e2ee.jpg'
 export default function Involucrate() {
   return (
-  <div>
-    <Portada titulo={"Dale con el corazon"} img={img} />
-    <Form_Especialistas/>
-  </div>
+    <div>
+      <Portada titulo={'Dale con el corazon'} img={img} />
+      <Form_Especialistas />
+      <Form_Instituciones />
+    </div>
   )
 }
