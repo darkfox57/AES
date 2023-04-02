47b88cb3a3273825fabeb831127f62f9b9ae6194
Andrés Pinto <andres.pinto.n@gmail.com>
2023-04-02 09:19:28 -0500

    update blog

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -7,33 +7,6 @@ import BlogCard from './BlogCard'
 import usePagination from '../../Hooks/usePagination'
 import BlogHeader from './BlogHeader'
 
-// DE PRUEBA ARRAY DE OBJETO
-const event = [
-  {
-    fecha: '02/45/24',
-    text: '10 señales de que tu grupo de apoyo no esta funcionando',
-  },
-  {
-    fecha: '02/45/24',
-    text: '7 señales de que tu grupo de apoyo no esta funcionando',
-  },
-  {
-    fecha: '02/45/24',
-    text: '8 señales de que tu grupo de apoyo no esta funcionando',
-  },
-  {
-    fecha: '02/45/24',
-    text: '10 señales de que tu grupo de apoyo no esta funcionando',
-  },
-  {
-    fecha: '02/45/1999',
-    text: '7 señales de que tu grupo de apoyo no esta funcionando',
-  },
-  {
-    fecha: '02/45/24',
-    text: '8 señales de que tu grupo de apoyo no esta funcionando',
-  },
-]
 const Blog = () => {
   // usamos un useHook perzonalizado para la paginacion tipo slider
 
