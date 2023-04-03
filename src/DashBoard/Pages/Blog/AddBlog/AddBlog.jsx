871ca20d47e63fdc419ee00fc3eae08eb5823db0
zLuciel <82103687+zLuciel@users.noreply.github.com>
2023-04-03 03:06:52 -0500

    AddblogForm
    agregado correctamente el form e incluido el dato ReactQuill

--- src/DashBoard/Pages/Blog/AddBlog/AddBlog.jsx
+++ src/DashBoard/Pages/Blog/AddBlog/AddBlog.jsx
@@ -2,19 +2,42 @@ import React, { useState } from 'react'
 import ReactQuill from 'react-quill'
 import 'react-quill/dist/quill.snow.css'
 import Button from '../../../../utils/Button/Button'
+import TextInput from '../../../../utils/TextInput/TextInput'
+//forms react
+import { useForm } from 'react-hook-form'
+import FileInput from '../../../../components/Form_Especialistas/FileInput'
 
 export default function AddBlog() {
   const [postContent, setPostContent] = useState('')
 
-  const handleSubmit = (event) => {
-    event.preventDefault()
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm()
+
+  const handleData = (data) => {
+    data.description = postContent // agregamos la descripcion
+    console.log(data)
     // Aquí puedes enviar el contenido del post al servidor
   }
-
+  // descipciones - imagen - nombre y categorias chexbox de un array de objetos category.name - titulo
+  const categorias = [
+    {
+      name: 'salud',
+    },
+    {
+      name: 'fisica',
+    },
+    {
+      name: 'mental',
+    },
+  ]
+  //*arreglar el scroll al cambiar la pagina para Andres **/
   return (
     <>
       <h2>Crear Articulo de Blog</h2>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit(handleData)}>
         <label>
           Descripción:
           <ReactQuill
@@ -22,6 +45,33 @@ export default function AddBlog() {
             onChange={(value) => setPostContent(value)}
           />
         </label>
+        {/**Input Nombre */}
+        <TextInput
+          label="Nombre"
+          type="text"
+          name="nombre"
+          register={register}
+          errors={errors}
+          required={true}
+          pattern="^[A-Za-zÁ-ÿ\s]+$"
+        />
+        {/**Categoria map checkbox */}
+        {categorias.map((category, index) => (
+          <TextInput
+            key={index}
+            type="checkbox"
+            register={register}
+            label={category.name}
+            value={category.name}
+            name={'categoria'}
+            errors={errors}
+            required={true}
+          />
+        ))}
+        {/**Categoria error  */}
+        {errors.categoria?.type === `required` && <p>Seleccione alguna categoria por favor</p>}
+        {/**Imagen  updale */}
+        <FileInput register={register} />
         <button type="submit">Publicar</button>
       </form>
     </>
--- src/utils/TextInput/TextInput.jsx
+++ src/utils/TextInput/TextInput.jsx
@@ -12,7 +12,7 @@ const TextInput = ({label, type,name,register,errors,value,required,pattern,maxL
         maxLength:maxLength || false,
         pattern: Regex
       })} />
-       {errors[name]?.type === `required` && type !== "radio" && <p>{`El campo ${label} es requerido`}</p>}
+       {errors[name]?.type === `required` && (type !== "radio" && type !== "checkbox") && <p>{`El campo ${label} es requerido`}</p>}
        {errors[name]?.type === `maxLength` && <p>{`El campo ${label} debe tener 15 digitos maximo`}</p>}
        {errors[name]?.type === `pattern` && <p>{`El formato del  ${label} es incorrecto`}</p>}
     </div>
