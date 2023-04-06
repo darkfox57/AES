054d03994c6a1333d1aa370ca508bbaa913c3404
mxv-0827 <maximilianoviand@gmail.com>
2023-04-06 18:36:19 -0300

    estiy implementando paises

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,10 +1,12 @@
-import { configureStore } from '@reduxjs/toolkit';
-import BlogReducer from '../reducer/blog_reducer';
-import EventReducer from '../reducer/event_reducer';
+import { configureStore } from '@reduxjs/toolkit'
+import BlogReducer from '../reducer/blog_reducer'
+import EventReducer from '../reducer/event_reducer'
+import FormReducer from '../reducer/form_reducer'
 
 export default configureStore({
- reducer: {
-  blog: BlogReducer,
-  event: EventReducer
- },
-});
+  reducer: {
+    blog: BlogReducer,
+    event: EventReducer,
+    form: FormReducer,
+  },
+})
