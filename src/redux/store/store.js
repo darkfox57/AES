9b9faa2b35094daa9025ce2e55f74f37ee19f440
Andrés Pinto <andres.pinto.n@gmail.com>
2023-04-06 18:50:32 -0500

    login

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,10 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
+import AccountReducer from '../reducer/account_reducer';
 import BlogReducer from '../reducer/blog_reducer';
 import EventReducer from '../reducer/event_reducer';
+import FormReducer from '../reducer/form_reducer';
 
 export default configureStore({
  reducer: {
   blog: BlogReducer,
-  event: EventReducer
+  event: EventReducer,
+  account: AccountReducer
  },
 });
