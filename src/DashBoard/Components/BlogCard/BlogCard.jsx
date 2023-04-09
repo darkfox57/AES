35a5754d3d12842b8c68567c48df5efbecc17d6f
Andrés Pinto <andres.pinto.n@gmail.com>
2023-04-09 13:57:22 -0500

    Blogs Edits And Status (#84)

--- src/DashBoard/Components/BlogCard/BlogCard.jsx
+++ src/DashBoard/Components/BlogCard/BlogCard.jsx
@@ -1,30 +1,72 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { editBlog } from '../../../redux/actions/blog_actions'
 import Button from '../../../utils/Button/Button'
-import { CardContainer, ContainerInfo } from './blogcard.styles'
+import { CardContainer, ToggleButton } from './blogcard.styles'
 
-export default function BlogCard({ image, title, date, status, id, slug }) {
+export default function BlogCard({
+  image,
+  title,
+  date,
+  status,
+  id,
+  slug,
+  categories,
+  description,
+  tags,
+}) {
+  const dispatch = useDispatch()
+  const [active, setActive] = useState(status)
+
+  const edited = useSelector((state) => state.blog.confirmation)
+
+  const handleClick = () => {
+    const post = {
+      id,
+      title,
+      description,
+      image,
+      status: !active,
+      categories: categories.map((c) => c._id),
+      tags: tags.map((t) => t._id),
+    }
+    dispatch(editBlog(post))
+    setActive(active ? false : true)
+  }
+
+  // useEffect(() => {
+  //   setActive(active ? false : true)
+  // }, [edited.message])
+
   return (
     <>
       <CardContainer>
         <img src={image} alt={title} />
-        <ContainerInfo>
-          <span>
-            {new Date(date).toLocaleString('es-ES', {
-              day: 'numeric',
-              month: 'numeric',
-              year: 'numeric',
-            })}{' '}
-          </span>
-          <p>{title}</p>
-          <div>
-            <Button
-              type="primary"
-              text="Editar"
-              size="lg"
-              link={`/dashboard/blog/${slug}`}
-            />
-          </div>
-        </ContainerInfo>
+        <p>{title}</p>
+        <span>
+          {new Date(date).toLocaleString('es-ES', {
+            day: 'numeric',
+            month: 'numeric',
+            year: 'numeric',
+          })}{' '}
+        </span>
+        <Button
+          type="primary"
+          text="Editar"
+          size="lg"
+          link={`/dashboard/blog/${slug}`}
+        />
+        <div className="toggle">
+          <ToggleButton
+            className={`${active ? ' active' : ''}`}
+            type="button"
+            onClick={handleClick}
+            aria-pressed={status}
+            autoComplete="off"
+          >
+            <div className="handle"></div>
+          </ToggleButton>
+        </div>
       </CardContainer>
     </>
   )
--- src/pages/Blog/Blog.jsx
+++ src/pages/Blog/Blog.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
-import {  useSelector } from 'react-redux'
+import { useSelector } from 'react-redux'
 import blogimg from '../../assets/About.webp'
 //import BlogCard from '../../components/Blog/BlogCard'
 import Footer from '../../components/Footer/Footer'
 import Portada from '../../components/Portada/Portada'
 //import { getAllBlogs, getCategories } from '../../redux/actions/blog_actions'
-import { BlogBody, ContainerMenuBlog, GridCardBlog } from './blog.styles'
-import BlogCardPage from '../../components/BlogCardPage/BlogCardPage'
-import SearchBlog from '../../components/MenuBlogPage/Search/SearchBlog'
 import usePagination from '../../Hooks/usePagination'
+import BlogCardPage from '../../components/BlogCardPage/BlogCardPage'
 import BtnPaginado from '../../components/BtnPaginado/BtnPaginado'
-import NoticiaDestacada from '../../components/MenuBlogPage/Noticia/NoticiaDestacada'
 import Categorias from '../../components/MenuBlogPage/Categoria/Categorias'
-import SiguenosRedes from '../../components/MenuBlogPage/Siguenos/SiguenosRedes'
 import EtiquetasPopular from '../../components/MenuBlogPage/Etiquetas/EtiquetasPopular'
+import NoticiaDestacada from '../../components/MenuBlogPage/Noticia/NoticiaDestacada'
+import SearchBlog from '../../components/MenuBlogPage/Search/SearchBlog'
+import SiguenosRedes from '../../components/MenuBlogPage/Siguenos/SiguenosRedes'
+import { BlogBody, ContainerMenuBlog, GridCardBlog } from './blog.styles'
 
 export default function Blog() {
   ///const dispatch = useDispatch()
@@ -24,7 +24,7 @@ export default function Blog() {
     dispatch(getAllBlogs())
     dispatch(getCategories())
   },[])*/
-  
+
   const {
     currentPage,
     totalPages,
@@ -36,33 +36,40 @@ export default function Blog() {
   } = usePagination(posts, 8)
 
   return (
-    console.log(posts),
     <>
       <Portada img={blogimg} titulo="Blog" />
       <BlogBody>
         <GridCardBlog>
-          {paginatedData.map((post, i) => (
-            <BlogCardPage
-              key={post._id}
-              img={post.image}
-              title={post.title}
-              status={post.status}
-              date={post.createdAt}
-              slug={post.slug}
-              description={post.description}
-            />
-          ))}
-          <BtnPaginado PreviousPage={PreviousPage} posts={posts} NextPage={NextPage} pageNumbers={pageNumbers} goToPage={goToPage} />
+          {paginatedData
+            .filter((post) => post.status)
+            .map((post, i) => (
+              <BlogCardPage
+                key={post._id}
+                img={post.image}
+                title={post.title}
+                status={post.status}
+                date={post.createdAt}
+                slug={post.slug}
+                description={post.description}
+              />
+            ))}
+          <BtnPaginado
+            PreviousPage={PreviousPage}
+            posts={posts}
+            NextPage={NextPage}
+            pageNumbers={pageNumbers}
+            goToPage={goToPage}
+          />
         </GridCardBlog>
         <ContainerMenuBlog>
           <SearchBlog />
-          <NoticiaDestacada/>
+          <NoticiaDestacada />
           <Categorias category={category} posts={posts} />
-          <SiguenosRedes/>
-          <EtiquetasPopular/>
+          <SiguenosRedes />
+          <EtiquetasPopular />
         </ContainerMenuBlog>
       </BlogBody>
-      <Footer/>
+      <Footer />
     </>
   )
 }
