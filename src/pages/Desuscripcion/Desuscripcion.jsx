1f8d8350ab3f6f762e8380d9409fd01d3d522681
mxv-0827 <maximilianoviand@gmail.com>
2023-04-17 17:37:01 -0300

    ruta desuscripcion

--- src/pages/Desuscripcion/Desuscripcion.jsx
+++ src/pages/Desuscripcion/Desuscripcion.jsx
@@ -1,33 +1,36 @@
 import React from 'react'
+import { useSearchParams } from 'react-router-dom'
 import { useDispatch } from 'react-redux'
 
 import { MainContainer } from './Desuscripcion.Styles'
 import { SubmitButton } from '../../utils/Form_Involucrate/Form_Involucrate.Styles'
 import { deleteFormSuscriptioin } from '../../redux/actions/form_actions'
 
-
 export default function Desuscripcion() {
+  const [searchparams] = useSearchParams()
+  const email = searchparams.get('email')
 
-  /*const DesuscripcionBtn = ()=> {
   const dispatch = useDispatch()
 
-    const handleSuscription = (email) => {
+  const handleDesuscription = () => {
     dispatch(deleteFormSuscriptioin(email))
-  }*/
-  
+  }
+
   return (
     <MainContainer>
       <h3>Cancelacion</h3>
 
       <span>
         Haga clic en "Cancelar suscripción" para dejar de recibir mensajes de
-        este remitente a esta dirección de correo electrónico:
-        Haga clic en "Cancelación" para dejar de recibir mensajes de este
-        remitente a esta dirección de correo electrónico:
+        este remitente a esta dirección de correo electrónico: Haga clic en
+        "Cancelación" para dejar de recibir mensajes de este remitente a esta
+        dirección de correo electrónico:
         <strong></strong>
       </span>
 
-      <SubmitButton>Cancelar suscripcion</SubmitButton>
+      <SubmitButton onClick={handleDesuscription}>
+        Cancelar suscripcion
+      </SubmitButton>
     </MainContainer>
   )
 }
