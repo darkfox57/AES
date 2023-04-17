0846e1003577ed6c6cc887a16d6ae778620dff93
mxv-0827 <maximilianoviand@gmail.com>
2023-04-17 18:03:21 -0300

    Intento 4

--- src/pages/Desuscripcion/Desuscripcion.jsx
+++ src/pages/Desuscripcion/Desuscripcion.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import { useSearchParams } from 'react-router-dom'
 import { useDispatch } from 'react-redux'
 
+import Swal from 'sweetalert2'
+import withReactContent from 'sweetalert2-react-content'
+
 import { MainContainer } from './Desuscripcion.Styles'
 import { SubmitButton } from '../../utils/Form_Involucrate/Form_Involucrate.Styles'
 import { deleteFormSuscriptioin } from '../../redux/actions/form_actions'
@@ -11,9 +14,31 @@ export default function Desuscripcion() {
   const email = searchparams.get('email')
 
   const dispatch = useDispatch()
+  const MySwal = withReactContent(Swal)
 
   const handleDesuscription = () => {
-    dispatch(deleteFormSuscriptioin(email))
+    try {
+      dispatch(deleteFormSuscriptioin(email))
+      return notification()
+    } catch (error) {
+      errorNotify()
+    }
+  }
+
+  const notification = async () => {
+    await MySwal.fire({
+      icon: 'success',
+      title: 'Proceso exitoso',
+      text: 'Te has desuscripto exitosamente',
+    })
+  }
+
+  const errorNotify = async () => {
+    await MySwal.fire({
+      icon: 'error',
+      title: 'Oops...',
+      text: 'Lo sentimos, el proceso de desuscripcion ha fallado!',
+    })
   }
 
   return (
