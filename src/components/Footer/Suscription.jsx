a07b386fe6522ceb13aab14930f0c66ab9460cb9
mxv-0827 <maximilianoviand@gmail.com>
2023-04-18 00:04:58 -0300

    Ventanas modales encaminadas
    Aun faltan cosas pero mañana las sigo

--- src/components/Footer/Suscription.jsx
+++ src/components/Footer/Suscription.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { useDispatch } from 'react-redux'
+import React, { useState, useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { useForm } from 'react-hook-form'
 
 import Swal from 'sweetalert2'
@@ -13,23 +13,29 @@ import { FormContainer } from './Suscription.Styles'
 import { addFormSuscription } from '../../redux/actions/form_actions'
 
 export default function () {
+  // const [showError, setShowError] = useState(false)
+
   const {
     register,
     handleSubmit,
-    control,
     formState: { errors },
   } = useForm()
   const dispatch = useDispatch()
+  const error = useSelector((state) => state.form.error)
   const MySwal = withReactContent(Swal)
 
   const regexMail = new RegExp('^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$')
 
   const handleSuscription = (data) => {
-    try {
-      dispatch(addFormSuscription(data))
+    dispatch(addFormSuscription(data))
+    // .then(() => setShowError(false))
+    // .catch(() => setShowError(true))
+
+    // console.log(error)
+    if (error) {
+      return errorNotify()
+    } else {
       return notification()
-    } catch (error) {
-      errorNotify()
     }
   }
 
@@ -41,13 +47,21 @@ export default function () {
     })
   }
 
-  const errorNotify = async ({ errorMsg }) => {
+  const errorNotify = async () => {
     await MySwal.fire({
       icon: 'error',
       title: 'Oops...',
-      text: errorMsg,
+      text: 'Lo sentimos, parece que hubo un problema al querer inscribirte al newsletter',
     })
   }
+
+  // useEffect(() => {
+  //   if (showError) {
+  //     errorNotify()
+  //   } else {
+  //     notification()
+  //   }
+  // }, [showError])
 
   return (
     <FormContainer onSubmit={handleSubmit(handleSuscription)}>
