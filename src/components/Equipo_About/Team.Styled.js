d01c490adee5f20019e29c6b3dc451b816d67659
Andrés Pinto <andres.pinto.n@gmail.com>
2023-04-19 12:29:27 -0500

    video fixes (#179)

--- src/components/Equipo_About/Team.Styled.js
+++ src/components/Equipo_About/Team.Styled.js
@@ -3,12 +3,19 @@ import styled from 'styled-components'
 export const TeamFlex = styled.div`
   display: flex;
   flex-direction: column;
+  text-align: center;
+  color: var(--text-white);
+  background: var(--primary-blue);
+  padding: var(--section-padding);
+  gap: 32px;
+.teamflex {
+  display: flex;
+  flex-direction: column;
   row-gap: 50px;
   justify-content: center;
   align-items: center;
-  background: rgb(0, 186, 163);
+  background: var(--primary-blue);
   height: max-content;
-  padding: var(--section-padding);
   & > div:first-child { 
     display: flex;
     justify-content: center;
@@ -19,5 +26,6 @@ export const TeamFlex = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
+  }
   }
 `
